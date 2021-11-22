70e7cb8f1adc3941d3b7a6e34a4a19d59f3a740f
Hritik Pawar <73632558+unlikelycreator@users.noreply.github.com>
2021-11-22 19:26:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App bg-gray-900 text-white flex items-center justify-center">
+      <h1 className="text-4xl">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
